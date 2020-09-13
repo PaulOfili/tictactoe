777e9bcdc35445af948780d5f79be7f4e35ef01a
Paul Ofili <paulofili42@gmail.com>
2020-09-13 23:53:30 +0100

    Updated tictatoe version

--- tictactoe-cli/index.js
+++ tictactoe-cli/index.js
@@ -1,5 +1,6 @@
 const gameReadline = require('readline-sync');
 const { Tictactoe, TictactoeWithAI } = require('@paulofili/tictactoe');
+// const { Tictactoe, TictactoeWithAI } = require('../tictactoe-driver');
 
 const DEFAULT_BOARD_SIZE = 3;
 
@@ -12,6 +13,27 @@ const getCorrectResponse = () => {
             return response;
         }
     }
+}
+
+const showBoard = (board) => {
+    const size = board.length;
+
+    let gameBoard = "\n";
+    for (let i = 0; i < size; ++i) {
+        let eachRow = " ";
+        for (let j = 0; j < size; ++j) {
+            eachRow += `${(board[i][j]) ? board[i][j] : "-"}`;
+            if (j < size - 1) {
+                eachRow += " | ";
+            }
+        }
+        if (i < size - 1) {
+            eachRow += "\n-----------\n";
+        }
+        gameBoard += eachRow;
+    }
+    gameBoard += "\n";
+    console.log(gameBoard);
 }
 
 console.log("Welcome to tictactoe");
@@ -27,32 +49,35 @@ if (response === 1) {
     tictactoe = new Tictactoe(DEFAULT_BOARD_SIZE)
 } else {
 
-    console.log("I see you got some guts, I like that. Do you want computer to play first.\n1. Yes (I am a rockstar)\n2. No ( scaredy cat)\n")
+    console.log("I see you got some guts, I like that. Do you want computer to play first.\n1. Yes (I am a rockstar)\n2. No (scaredy cat)\n")
 
     response = getCorrectResponse();
 
     if (response === 1) {
-        tictactoe = new TictactoeWithAI(DEFAULT_BOARD_SIZE, "X");
+        tictactoe = new TictactoeWithAI("X");
     } else {
-        tictactoe = new TictactoeWithAI(DEFAULT_BOARD_SIZE, "O")
+        tictactoe = new TictactoeWithAI("O")
     }
 }
+tictactoe.on("showBoard",  () => {
+    showBoard(tictactoe.board);
+})
 
-tictactoe.showBoard()
+tictactoe.on("cellTaken", () => {
+    console.log("Cell already taken, choose a different!")
+})
+
+
+showBoard(tictactoe.board);
 
 while(tictactoe.isGameRunning) { 
-    let position = gameReadline.questionInt('Pick a position on the board to play? ');
+    let position = gameReadline.questionInt('Pick a position on the board to play? From 1 - 9: ');
     position = Number(position)
     if (!Number.isInteger(position) || (position < 1 || position > 9)) {
         console.log("Invalid position");
         continue;
     }
-
-    if(tictactoe.playPiece(position) === false){
-        // console.log("Cell already taken, choose a different!")    
-    }
-
-    tictactoe.showBoard();
+    tictactoe.playPiece(position)
 }
 
 if (tictactoe.winner !== null) {
@@ -60,3 +85,7 @@ if (tictactoe.winner !== null) {
 } else {
     console.log("This is a draw");
 }
+
+tictactoe.off("showBoard", () => {
+    console.log("The end")
+});
